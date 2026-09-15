2968adac9cd7903f2ad3c32d0e91da4828800326
betse <betse11001@gmail.com>
2026-09-15 18:04:43 +0200

    ci: deploy to GitHub Pages

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 export const routes = {
   landing: "/",
@@ -8,7 +8,7 @@ export const routes = {
 } as const;
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: routes.landing,
